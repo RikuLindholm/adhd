09decbd064ae6b6ef7205429698e191c3a99a643
tsivula <tuosiv@gmail.com>
2013-04-16 22:19:40 +0300

    Update dhtpacket.h

--- dhtpacket.h
+++ dhtpacket.h
@@ -13,18 +13,20 @@ typedef struct DHTPacket {
   unsigned char *data;
 } DHTPacket;
 
-DHTPacket *create_packet(unsigned char *destination, unsigned char *origin,
+// Creates a new DHTPacket by binding (not copying) all the given parameters
+DHTPacket *bind_packet(unsigned char *destination, unsigned char *origin,
                         unsigned short type, unsigned short length,
                         unsigned char *data);
 
+// Destroys the given packet
 void destroy_packet(DHTPacket *pkt);
 
+// Creates a char array representation of the given parameters
 char *encode_packet(unsigned char *destination, unsigned char *origin,
                     unsigned short type, unsigned short length,
                     unsigned char *data);
 
+// Creates a char array representation of the given package
 char *serialize_packet(DHTPacket *pkt);
-
-DHTPacket *deserialize_packet(char *);
 
 #endif
