13009d7519ade9eef034eb4e5a673748d094b571
tsivula <tuosiv@gmail.com>
2013-04-16 22:19:14 +0300

    Update dhtpacket.c

--- dhtpacket.c
+++ dhtpacket.c
@@ -1,36 +1,20 @@
 #include "dhtpacket.h"
 
-DHTPacket *create_packet(unsigned char *destination, unsigned char *origin,
-                        unsigned short type, unsigned short length,
-                        unsigned char *data)
+DHTPacket *bind_packet(unsigned char *destination, unsigned char *origin,
+                                unsigned short type, unsigned short length,
+                                unsigned char *data)
 {
   DHTPacket *packet = malloc(sizeof(struct DHTPacket));
-
-  // Allocate attributes
-  packet->destination = (unsigned char *) malloc(strlen((char *) destination));
-  packet->origin = (unsigned char *) malloc(strlen((char *) origin));
-
-  if (packet == NULL || packet->destination == NULL || packet->origin == NULL)
-    return NULL;
-
-  // Set attributes
-  strcpy((char *) packet->destination, (char *) destination);
-  strcpy((char *) packet->origin, (char *) origin);
+  packet->destination = destination;
+  packet->origin = origin;
   packet->type = type;
   packet->length = length;
-
-  // Only allocate data if it exists
-  if (data != NULL) {
-    packet->data = (unsigned char *) malloc(strlen((char *) data));
-    strcpy((char *) packet->data, (char *) data);
-  } else {
-    packet->data = NULL;
-  }
-
+  packet->data = data;
   return packet;
 }
 
-void destroy_packet(DHTPacket *pkt) {
+void destroy_packet(DHTPacket *pkt)
+{
   if (pkt == NULL) return;
   free(pkt->destination);
   free(pkt->origin);
@@ -56,24 +40,9 @@ char *encode_packet(unsigned char *destination, unsigned char *origin,
 }
 
 /* Converts the given DHTPacket-structure into a byte array.*/
-char *serialize_packet(DHTPacket *packet) {
+char *serialize_packet(DHTPacket *packet)
+{
   return encode_packet(packet->destination, packet->origin,
     packet->type, packet->length, packet->data);
 }
 
-/* Converts the given byte array into a DHTPacket-structure.*/
-DHTPacket *deserialize_packet(char *data) {
-  unsigned char *destination = malloc(20*sizeof(char));
-  memcpy(destination, data, 20);
-  unsigned char *origin = malloc(20*sizeof(char));
-  memcpy(origin, data + 20, 20);
-  unsigned short type = ((data[40] & 0xff) << 8) | (data[41] & 0xff);
-  unsigned short length = ((data[42] & 0xff) << 8) | (data[43] & 0xff);
-  unsigned char *payload = malloc(length*sizeof(char));
-  memcpy(payload, data + 44, length);
-  DHTPacket *pkt = create_packet(destination, origin, type, length, payload);
-  free(destination);
-  free(origin);
-  free(payload);
-  return pkt;
-}
